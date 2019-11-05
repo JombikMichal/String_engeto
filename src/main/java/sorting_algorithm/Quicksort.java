279d5df45ac8b96cf2e9ebc1b97d40319f6a74ae
Michal <mjombik@kkmbfin.sk>
2019-11-05 23:30:32 +0100

    quicksort description

--- src/main/java/sorting_algorithm/Quicksort.java
+++ src/main/java/sorting_algorithm/Quicksort.java
@@ -39,6 +39,32 @@ public class Quicksort implements Sort{
         return result;
     }
 
+    /**
+     * Quicksort is a sorting algorithm, which is leveraging the divide-and-conquer principle.
+     * It has an average O(n log n) complexity and it’s one of the most used sorting algorithms, especially for big data volumes.
+     *
+     * It's important to remember that Quicksort isn't a stable algorithm.
+     * A stable sorting algorithm is an algorithm where the elements with the same values appear in the same order in the sorted output as they appear in the input list.
+     *
+     * The input list is divided into two sub-lists by an element called pivot; one sub-list with elements less than the pivot and another one with elements greater than the pivot.
+     * This process repeats for each sub-list.
+     *
+     * Finally, all sorted sub-lists merge to form the final output.
+     *
+     * We choose an element from the list, called the pivot. We'll use it to divide the list into two sub-lists.
+     * We reorder all the elements around the pivot – the ones with smaller value are placed before it, and all the elements greater than the pivot after it.
+     * After this step, the pivot is in its final position. This is the important partition step.
+     * We apply the above steps recursively to both sub-lists on the left and right of the pivot.
+     *
+     * Let's suppose we pick 5 as the pivot for simplicity
+     * We'll first put all elements less than 5 in the first position of the array: {3, 4, 5, 6, 5, 9}
+     * We'll then repeat it for the left sub-array {3,4}, taking 3 as the pivot
+     * There are no elements less than 3
+     * We apply quicksort on the sub-array in the right of the pivot, i.e. {4}
+     * This sub-array consists of only one sorted element
+     * We continue with the right part of the original array, {6, 5, 9} until we get the final ordered array
+     * 
+     * */
     @Override
     public int[] sort() {
 
